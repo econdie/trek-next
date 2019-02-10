efdedf9f3ae790be5c878932134c632cd3a7cb49
econdie <erik@7cups.com>
2019-02-10 14:58:22 -0500

    add linter/prettier

---
+++ www/next-seo.config.js
@@ -0,0 +1,14 @@
+export default {
+  title: "Trek Next",
+  titleTemplate: `%s | TrekNext`,
+  description: "What will be your next trek?",
+  noindex: true,
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: "https://www.treknext.com/",
+    title: "Trek Next",
+    description: "What will be your next trek?",
+    site_name: "Trek Next"
+  }
+};
