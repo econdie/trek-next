47b8ec3bae778962de325d58e6c70eeaea2c09e9
econdie <erik@7cups.com>
2019-02-17 16:09:32 -0500

    gofmt

--- api/model/model.go
+++ api/model/model.go
@@ -2,7 +2,7 @@ package model
 
 //StandardResponse - all responses should use this struct
 type StandardResponse struct {
-	Status  int                     `json:"status"`  //response status code, 200 for OK, etc.
-	Payload map[string]interface{}  `json:"payload"` //struct for request specific data, can be null if not applicable
-	Message string                  `json:"message"` //optional message text
+	Status  int                    `json:"status"`  //response status code, 200 for OK, etc.
+	Payload map[string]interface{} `json:"payload"` //struct for request specific data, can be null if not applicable
+	Message string                 `json:"message"` //optional message text
 }
